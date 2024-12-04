aa2547d02acc9c0602c9f6bde4fc235da5268021
MirtaAlfalunaKaban <lunakaban7@gmail.com>
2024-12-04 12:12:22 +0700

    Tugas Pertemuan10 DDP Mirta Alfaluna Kaban

---
+++ panggil.py
@@ -0,0 +1,23 @@
+import BangunDatar
+
+BangunDatar.l_persegi(6)
+BangunDatar.l_persegi_panjang(10,5)
+BangunDatar.l_segitiga(6, 10)
+BangunDatar.l_jajargenjang(8, 4)
+BangunDatar.l_lingkaran(8)
+
+import BangunRuang
+
+BangunRuang.l_balok(4,6,8)
+BangunRuang.l_kubus(4)
+BangunRuang.l_limas(10, 5)
+BangunRuang.l_prisma(6, 4)
+BangunRuang.l_tabung(6, 8)
+
+import hitung
+
+hitung.tambah(50, 60)
+hitung.kurang(70, 25)
+hitung.kali(4, 70)
+hitung.bagi(75, 6)
+hitung.pangkat(8, 6)
